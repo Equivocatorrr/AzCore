507d70ab5ee9a3b94af5d45d448e3d8866b302d5
Philip Haynes <singularity.haynes@gmail.com>
2019-01-26 01:11:10 -0600

    Fixed mingw bug that determines u64 differently

--- src/basictypes.hpp
+++ src/basictypes.hpp
@@ -9,7 +9,13 @@
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
+#ifdef __unix
 typedef unsigned long u64;
+#else
+#ifdef __MINGW32__
+typedef unsigned long long u64;
+#endif
+#endif
 typedef char i8;
 typedef short i16;
 typedef int i32;

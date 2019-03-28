bad7d7f8056752201033b9b308c9cc825b281fd2
Philip Haynes <singularity.haynes@gmail.com>
2019-03-27 19:28:09 -0500

    Now only use long long for 64-bit ints

--- src/basictypes.hpp
+++ src/basictypes.hpp
@@ -9,17 +9,11 @@
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
-#ifdef __unix
-typedef unsigned long u64;
-#else
-#ifdef __MINGW32__
 typedef unsigned long long u64;
-#endif
-#endif
 typedef char i8;
 typedef short i16;
 typedef int i32;
-typedef long i64;
+typedef long long i64;
 typedef float f32;
 typedef double f64;
 

2323fd962fe7acada747cb338b603ffaa56596e6
Philip Haynes <singularity.haynes@gmail.com>
2019-09-26 18:27:50 -0500

    Packing scripts now pack libraries and OpenAL32.dll gets packed

--- projects/PackLinux.sh
+++ projects/PackLinux.sh
@@ -1,13 +1,27 @@
 #Used to easily package the binaries and their libraries
 echo "Packing everything needed to distribute on Linux . . ."
-rm -r Exports/Linux*
+if [ -e Exports ]
+then
+    rm -r Exports/Linux*
+fi
 
 mkdir -p Exports/Linux
 
-cp -r data Exports/Linux/data
+if [ -e data ]
+then
+    cp -r data Exports/Linux/data
+fi
 
 cp bin/Linux/Release/$1 Exports/Linux/Release
 cp bin/Linux/Debug/$1 Exports/Linux/Debug
+
+if [ -e lib ]
+then
+    mkdir -p Exports/Linux/lib
+    for filename in lib/*.so*; do
+        cp "$filename" Exports/Linux/lib
+    done
+fi
 
 cd Exports
 echo "Compressing into a tar ball . . ."
--- projects/PackWindows.sh
+++ projects/PackWindows.sh
@@ -1,15 +1,28 @@
 #Used to easily package the binaries and their libraries
 echo "Packing everything needed to distribute on Windows . . ."
-rm -r Exports/Windows*
+if [ -e Exports ]
+then
+    rm -r Exports/Windows*
+fi
 
 mkdir -p Exports/Windows
 
-cp -r data Exports/Windows/data
+if [ -e data ]
+then
+    cp -r data Exports/Windows/data
+fi
 
 cp bin/Windows/Release/$1.exe Exports/Windows/Release.exe
 cp bin/Windows/Debug/$1.exe Exports/Windows/Debug.exe
 
 cp ../../base/lib/libgcc_s_sjlj-1.dll Exports/Windows
+
+if [ -e lib ]
+then
+    for filename in lib/*.dll; do
+        cp "$filename" Exports/Windows
+    done
+fi
 
 cd Exports
 echo "Compressing into a zip archive . . ."

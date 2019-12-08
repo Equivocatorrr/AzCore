5ee73fb3809ecd7a21c10c2ea28825640dfb3ee4
Philip Haynes <singularity.haynes@gmail.com>
2019-12-08 02:23:45 -0600

    Add SeparateByValues

--- base/src/AzCore/memory.hpp
+++ base/src/AzCore/memory.hpp
@@ -43,6 +43,19 @@ using WeakPtr = std::weak_ptr<T>;
 
 Array<char> FileContents(String filename);
 
+template<typename T>
+Array<Range<T>> SeparateByValues(Array<T> *array, const Array<T> &values) {
+    Array<Range<T>> result;
+    i32 rangeStart = 0;
+    for (i32 i = 0; i < array->size; i++) {
+        if (values.Contains(array->data[i])) {
+            result.Append(array->GetRange(rangeStart, i-rangeStart));
+            rangeStart = i+1;
+        }
+    }
+    return result;
+}
+
 } // namespace AzCore
 
 #endif // AZCORE_MEMORY_HPP

1cec31ccf5985e6a5aa40bb1ec4b74345283ab54
Philip Haynes <singularity.haynes@gmail.com>
2021-06-21 18:42:12 -0400

    Add System Environment utilities

---
+++ base/src/AzCore/Environment.cpp
@@ -0,0 +1,76 @@
+/*
+    File: Environment.cpp
+    Author: Philip Haynes
+*/
+
+#include "Environment.hpp"
+#include <cstdlib>
+
+namespace AzCore {
+
+String ConfigDir() {
+    String out;
+    #ifdef __unix
+        char *var = getenv("HOME");
+        if (var) {
+            out = var;
+            out += "/.config/";
+        }
+    #elif defined(_WIN32)
+        char *var = getenv("APPDATA");
+        if (var) {
+            out = var;
+            out += "/";
+        }
+    #endif
+    return out;
+}
+
+String DataDir() {
+    String out;
+    #ifdef __unix
+        char *var = getenv("HOME");
+        if (var) {
+            out = var;
+            out += "/.local/share/";
+        }
+    #elif defined(_WIN32)
+        char *var = getenv("LOCALAPPDATA");
+        if (var) {
+            out = var;
+            out += "/";
+        }
+    #endif
+    return out;
+}
+
+bool isSlash(char c) {
+    return c == '\\' || c == '/';
+}
+
+void CleanFilePath(String *path) {
+    i32 lastDir = -1;
+    for (i32 i = 0; i < path->size-3; i++) {
+        char c1 = (*path)[i];
+        char c2 = (*path)[i+1];
+        char c3 = (*path)[i+2];
+        char c4 = (*path)[i+3];
+        if (isSlash(c1) && c2 == '.') {
+            if (c3 == '.' && isSlash(c4)) {
+                // dir/../dir
+                // ^^^^^^^
+                path->Erase(lastDir+1, i+3 - lastDir);
+            } else if (isSlash(c3)) {
+                // dir/./dir
+                //    ^^
+                path->Erase(i, 2);
+            }
+        } else {
+            if (isSlash(c1)) {
+                lastDir = i;
+            }
+        }
+    }
+}
+
+} // namespace AzCore
---
+++ base/src/AzCore/Environment.hpp
@@ -0,0 +1,22 @@
+/*
+    File: Environment.hpp
+    Author: Philip Haynes
+    Utilities for dealing with filesystem paths.
+*/
+
+#ifndef AZCORE_ENVIRONMENT_HPP
+#define AZCORE_ENVIRONMENT_HPP
+
+#include "Memory/String.hpp"
+
+namespace AzCore {
+
+String ConfigDir();
+
+String DataDir();
+
+void CleanFilePath(String *path);
+
+} // namespace AzCore
+
+#endif // AZCORE_ENVIRONMENT_HPP

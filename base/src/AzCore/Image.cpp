8d4752c9a907490bdbf9d0eb50d186e550c53eb8
Philip Haynes <singularity.haynes@gmail.com>
2021-09-18 18:46:43 -0400

    Implement AzCore::Image

---
+++ base/src/AzCore/Image.cpp
@@ -0,0 +1,122 @@
+/*
+	File: Image.cpp
+	Author: Philip Haynes
+*/
+
+// #define pow(v, e) pow((double)v, (double)e)
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
+// #undef pow
+
+#include "Image.hpp"
+#include "math.hpp"
+#include "memory.hpp"
+
+namespace AzCore {
+
+Image::Image(Image &other) {
+	if (other.pixels == nullptr) {
+		pixels = nullptr;
+	} else {
+		size_t size = other.height * other.stride;
+		pixels = (u8*)stbi__malloc(size);
+		memcpy(pixels, other.pixels, size);
+	}
+	width = other.width;
+	height = other.height;
+	channels = other.channels;
+	stride = other.stride;
+}
+
+Image::Image(const char *filename, i32 channelsDesired) :
+pixels(stbi_load(filename, &width, &height, &channels, channelsDesired))
+{
+	channels = channelsDesired;
+	stride = width * channels;
+}
+
+Image::~Image() {
+	Dealloc();
+}
+
+void Image::Alloc(i32 w, i32 h, i32 c, i32 s) {
+	Dealloc();
+	width = w;
+	height = h;
+	channels = c;
+	if (s) {
+		stride = s;
+	} else {
+		stride = width*c;
+	}
+	Alloc();
+}
+
+void Image::Alloc() {
+	size_t size = height * stride;
+	pixels = (u8*)malloc(size);
+}
+
+bool Image::Copy(u8 *buffer, i32 w, i32 h, i32 c, Image::Format fmt, i32 s, u8 def) {
+	if (nullptr == pixels) return false;
+	if (s == 0) s = w*c;
+	format = fmt;
+	i32 copyHeight = min(h, height);
+	i32 copyWidth = min(w, width);
+	if (s == stride && c == channels) {
+		memcpy(pixels, buffer, copyHeight*s);
+	} else if (c == channels) {
+		for (i32 y = 0; y < copyHeight; y++) {
+			memcpy(&pixels[y*stride], &buffer[y*s], copyHeight*copyWidth*c);
+		}
+	} else {
+		// Pain
+		i32 copyChannels = min(c, channels);
+		for (i32 y = 0; y < copyHeight; y++) {
+			for (i32 x = 0; x < copyWidth; x++) {
+				size_t dst = y*stride+x*channels;
+				memcpy(&pixels[dst], &buffer[y*s + x*c], copyChannels);
+				for (i32 cc = c; cc < channels; cc++) {
+					pixels[dst+cc] = def;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+void Image::Dealloc() {
+	if (pixels) {
+		free(pixels);
+		pixels = nullptr;
+	}
+}
+
+bool Image::Load(const char *filename, i32 channelsDesired) {
+	pixels = stbi_load(filename, &width, &height, &channels, channelsDesired);
+	channels = channelsDesired;
+	stride = width * channels;
+	format = RGBA;
+	return pixels != nullptr;
+}
+
+void Image::Reformat(Image::Format newFormat) {
+	if (format == newFormat) return;
+	for (i32 y = 0; y < height; y++) {
+		u8 *line = &pixels[y*stride];
+		for (i32 x = 0; x < width; x++) {
+			u8 *buf = &line[x*channels];
+			// Swap R and B
+			Swap(buf[0], buf[2]);
+		}
+	}
+}
+
+bool Image::SavePNG(const char *filename) {
+	Reformat(RGBA);
+	return stbi_write_png(filename, width, height, channels, pixels, stride);
+}
+
+} // namespace AzCore
---
+++ base/src/AzCore/Image.hpp
@@ -0,0 +1,50 @@
+/*
+	File: Image.hpp
+	Author: Philip Haynes
+	A shallow wrapper around stb_image.h
+*/
+
+#ifndef AZCORE_IMAGE_HPP
+#define AZCORE_IMAGE_HPP
+
+#include "basictypes.hpp"
+
+namespace AzCore {
+
+/*  struct: Image
+	Author: Philip Haynes
+	Allows loading and saving image files in many file formats, courtesy of stb_image */
+struct Image {
+	u8* pixels = nullptr;
+	i32 width = 0;
+	i32 height = 0;
+	i32 channels = 0;
+	i32 stride = 0;
+	enum Format {
+		RGBA,
+		BGRA
+	} format=RGBA;
+	Image() = default;
+	Image(Image &other);
+	inline Image(Image &&other) {
+		pixels = other.pixels;
+		other.pixels = nullptr;
+		width = other.width;
+		height = other.height;
+		channels = other.channels;
+	}
+	Image(const char *filename, i32 channelsDesired = 4);
+	~Image();
+	// width, height, channels, stride(defaults to width*channels)
+	void Alloc(i32 w, i32 h, i32 c, i32 s=0);
+	void Alloc();
+	void Dealloc();
+	bool Copy(u8 *buffer, i32 w, i32 h, i32 c, Format fmt, i32 s=0, u8 def=0);
+	bool Load(const char *filename, i32 channelsDesired = 4);
+	void Reformat(Format newFormat);
+	bool SavePNG(const char *filename);
+};
+
+} // namespace AzCore
+
+#endif // AZCORE_IMAGE_HPP

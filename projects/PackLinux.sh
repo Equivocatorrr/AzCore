1ab3e33095491654549e3774770be3b2b26acb89
Philip Haynes <singularity.haynes@gmail.com>
2021-09-26 17:59:31 -0400

    Change folder and archive naming to match project name

--- projects/PackLinux.sh
+++ projects/PackLinux.sh
@@ -1,29 +1,30 @@
 #Used to easily package the binaries and their libraries
 echo "Packing everything needed to distribute on Linux . . ."
+EXPORT_DIR=Exports/$1_Linux
 if [ -e Exports ]
 then
-	rm -r Exports/Linux*
+	rm -r $EXPORT_DIR*
 fi
 
-mkdir -p Exports/Linux
+mkdir -p $EXPORT_DIR
 
 if [ -e data ]
 then
-	cp -r data Exports/Linux/data
+	cp -r data $EXPORT_DIR/data
 fi
 
-cp bin/Linux/Release/$1 Exports/Linux/Release
-cp bin/Linux/Debug/$1 Exports/Linux/Debug
+cp bin/Linux/Release/$1 $EXPORT_DIR/$1
+cp bin/Linux/Debug/$1 $EXPORT_DIR/$1_Debug
 
 if [ -e lib ]
 then
-	mkdir -p Exports/Linux/lib
+	mkdir -p $EXPORT_DIR/lib
 	for filename in lib/*.so*; do
-		cp "$filename" Exports/Linux/lib
+		cp "$filename" $EXPORT_DIR/lib
 	done
 fi
 
 cd Exports
 echo "Compressing into a tar ball . . ."
-tar -czvf Linux.tar.gz Linux
+tar -czvf $1.tar.gz $1_Linux
 echo "Done!"
--- projects/PackWindows.sh
+++ projects/PackWindows.sh
@@ -1,30 +1,31 @@
 #Used to easily package the binaries and their libraries
 echo "Packing everything needed to distribute on Windows . . ."
+EXPORT_DIR=Exports/$1_Win
 if [ -e Exports ]
 then
-	rm -r Exports/Windows*
+	rm -r $EXPORT_DIR*
 fi
 
-mkdir -p Exports/Windows
+mkdir -p $EXPORT_DIR
 
 if [ -e data ]
 then
-	cp -r data Exports/Windows/data
+	cp -r data $EXPORT_DIR/data
 fi
 
-cp bin/Windows/Release/$1.exe Exports/Windows/Release.exe
-cp bin/Windows/Debug/$1.exe Exports/Windows/Debug.exe
+cp bin/Windows/Release/$1.exe $EXPORT_DIR/$1.exe
+cp bin/Windows/Debug/$1.exe $EXPORT_DIR/$1_Debug.exe
 
 # cp ../../base/lib/libgcc_s_sjlj-1.dll Exports/Windows
 
 if [ -e lib ]
 then
 	for filename in lib/*.dll; do
-		cp "$filename" Exports/Windows
+		cp "$filename" $EXPORT_DIR
 	done
 fi
 
 cd Exports
 echo "Compressing into a zip archive . . ."
-zip -r Windows.zip Windows
+zip -r $1.zip $1_Win
 echo "Done!"

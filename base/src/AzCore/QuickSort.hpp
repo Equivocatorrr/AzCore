fe5646139818c062d4192f1d93e2b867dadc766a
Philip Haynes <singularity.haynes@gmail.com>
2022-09-30 17:20:55 -0400

    Add QuickSort.hpp

---
+++ base/src/AzCore/QuickSort.hpp
@@ -0,0 +1,56 @@
+/*
+	File: QuickSort.hpp
+	Author: Philip Haynes
+	Does what it says on the tin.
+*/
+
+#ifndef AZCORE_QUICKSORT_HPP
+
+#include "basictypes.hpp"
+#include "memory.hpp"
+
+namespace AzCore {
+
+template<typename T, typename LessThanFunc_t>
+void QuickSort(SimpleRange<T> toSort, const LessThanFunc_t &lessThan) {
+	if (toSort.size < 2) return; // Nothing to be done
+	if (toSort.size == 2) {
+		if (lessThan(toSort[1], toSort[0])) {
+			Swap(toSort[0], toSort[1]);
+		}
+		return;
+	}
+	T pivot;
+	{
+		// Choose pivot
+		i64 mid = toSort.size/2;
+		i64 lo = 0;
+		i64 hi = toSort.size-1;
+		if (lessThan(toSort[mid], toSort[lo]))
+			Swap(lo, mid); // lo < mid
+		if (lessThan(toSort[hi], toSort[lo]))
+			Swap(lo, hi);  // lo < hi & mid
+		if (lessThan(toSort[hi], toSort[mid]))
+			Swap(mid, hi); // lo < mid < hi
+		pivot = toSort[mid];
+	}
+	i64 left = -1;
+	i64 right = toSort.size;
+	while (true) {
+		do {
+			left++;
+		} while (lessThan(toSort[left], pivot));
+		do {
+			right--;
+		} while (lessThan(pivot, toSort[right]));
+		if (left >= right) break;
+		Swap(toSort[left], toSort[right]);
+	}
+	i64 split = right+1;
+	QuickSort(toSort.SubRange(0, split), lessThan);
+	QuickSort(toSort.SubRange(split, toSort.size-split), lessThan);
+}
+
+} // namespace AzCore
+
+#endif // AZCORE_QUICKSORT_HPP

9b48cbcb0a1124d9e8462d6bfba92fb4833f5169
SingularityAzure <singularity.haynes@gmail.com>
2022-12-04 19:49:50 -0500

    Add f64 test that mirrors f32 test

--- projects/unit_tests/src/tests/String.cpp
+++ projects/unit_tests/src/tests/String.cpp
@@ -17,12 +17,13 @@ UT::Register stringTest("String", StringTest);
 using namespace AzCore;
 
 FPError<f32> fpError;
+FPError<f64> fp64Error;
 
 #define COMPARE_FP(lhs, rhs, magnitude) \
-	fpError.Compare(lhs, rhs, magnitude, __LINE__)
+	fpError.Compare(lhs, rhs, magnitude, __LINE__, String(), 0.0f, 1.0f)
 
 #define CHECK_F32_STRING(_real) \
-	real = (_real); \
+	real = f32(_real); \
 	str = ToString(real); \
 	UTExpectEquals(str, #_real); \
 	UTAssert(StringToF32(str, &real2)); \
@@ -33,6 +34,22 @@ FPError<f32> fpError;
 	str = ToString(real); \
 	UTAssert(StringToF32(str, &real2)); \
 	COMPARE_FP(real, real2, real);
+
+#define COMPARE_FP64(lhs, rhs, magnitude) \
+	fp64Error.Compare(lhs, rhs, magnitude, __LINE__, String(), 0.0, 1.0)
+
+#define CHECK_F64_STRING(_real) \
+	real = (_real); \
+	str = ToString(real); \
+	UTExpectEquals(str, #_real); \
+	UTAssert(StringToF64(str, &real2)); \
+	COMPARE_FP64(real, real2, real);
+
+#define CHECK_F64(_real) \
+	real = (_real); \
+	str = ToString(real); \
+	UTAssert(StringToF64(str, &real2)); \
+	COMPARE_FP64(real, real2, real);
 
 #define CHECK_I32_STRING(_integer) \
 	integer1 = (_integer); \
@@ -48,36 +65,70 @@ FPError<f32> fpError;
 	UTExpectEquals(integer1, integer2);
 
 void StringTest() {
-	f32 real, real2;
 	String str;
-
-	// float ToString and StringToF32
-	CHECK_F32_STRING(0.0f);
-	CHECK_F32_STRING(0.1f);
-	CHECK_F32_STRING(0.111f);
-	CHECK_F32_STRING(10.0f);
-	CHECK_F32_STRING(100.0f);
-	CHECK_F32_STRING(1000.0f);
-	real = 69.420f;
-	str = ToString(real, 10, 5);
-	UTExpectEquals(str, "69.42");
-	UTAssert(StringToF32(str, &real2));
-	COMPARE_FP(real, real2, real);
-	real = 1.0f / 10000.0f;
-	str = ToString(real);
-	UTExpectEquals(str, "1.0e-4");
-	UTAssert(StringToF32(str, &real2));
-	COMPARE_FP(real, real2, real);
-	real = 0.1f - 1.0f / 100000.0f;
-	str = ToString(real, 10, 2);
-	UTExpectEquals(str, "0.1");
-	UTAssert(StringToF32(str, &real2));
-	COMPARE_FP(0.1f, real2, 0.1f);
-
+	
 	RandomNumberGenerator rng(69420);
+	
+	{ // f32 ToString and StringToF32
+		f32 real, real2;
+		CHECK_F32_STRING(0.0);
+		CHECK_F32_STRING(0.1);
+		CHECK_F32_STRING(0.111);
+		CHECK_F32_STRING(10.0);
+		CHECK_F32_STRING(100.0);
+		CHECK_F32_STRING(1000.0);
+		real = 69.420f;
+		str = ToString(real, 10, 5);
+		UTExpectEquals(str, "69.42");
+		UTAssert(StringToF32(str, &real2));
+		COMPARE_FP(real, real2, real);
+		real = 1.0e-4f;
+		str = ToString(real);
+		UTExpectEquals(str, "1.0e-4");
+		UTAssert(StringToF32(str, &real2));
+		COMPARE_FP(real, real2, real);
+		real = 0.1f - 1.0f / 100000.0f;
+		str = ToString(real, 10, 2);
+		UTExpectEquals(str, "0.1");
+		UTAssert(StringToF32(str, &real2));
+		COMPARE_FP(0.1f, real2, 0.1f);
 
-	for (i32 i = 0; i < 100000; i++) {
-		CHECK_F32(random(-1000000.0f, 1000000.0f, &rng));
+		for (i32 i = 0; i < 100000; i++) {
+			f32 value = random(-1000000.0f, 1000000.0f, &rng);
+			CHECK_F32(value);
+		}
+	}
+	
+	{ // f64 ToString and StringToF64
+		f64 real, real2;
+		CHECK_F64_STRING(0.0);
+		CHECK_F64_STRING(0.1);
+		CHECK_F64_STRING(0.111);
+		CHECK_F64_STRING(10.0);
+		CHECK_F64_STRING(100.0);
+		CHECK_F64_STRING(1000.0);
+		real = 69.420;
+		str = ToString(real, 10, 5);
+		UTExpectEquals(str, "69.42");
+		UTAssert(StringToF64(str, &real2));
+		COMPARE_FP64(real, real2, real);
+		real = 1.0e-4;
+		str = ToString(real);
+		UTExpectEquals(str, "1.0e-4");
+		UTAssert(StringToF64(str, &real2));
+		COMPARE_FP64(real, real2, real);
+		real = 0.1 - 1.0 / 100000.0;
+		str = ToString(real, 10, 2);
+		UTExpectEquals(str, "0.1");
+		UTAssert(StringToF64(str, &real2));
+		COMPARE_FP64(0.1, real2, 0.1);
+
+		rng.Seed(69420);
+
+		for (i32 i = 0; i < 100000; i++) {
+			f64 value = random(-1000000.0, 1000000.0, &rng);
+			CHECK_F64(value);
+		}
 	}
 
 	// int ToString and StringToI32
@@ -88,6 +139,8 @@ void StringTest() {
 	CHECK_I32_STRING(2);
 	CHECK_I32_STRING(-1);
 	CHECK_I32_STRING(-2);
+	
+	rng.Seed(69420);
 
 	for (i32 i = 0; i < 100000; i++) {
 		CHECK_I32((i32)rng.Generate());
@@ -124,6 +177,7 @@ void StringTest() {
 	UTExpectEquals(str, "");
 
 	fpError.Report(__LINE__);
+	fp64Error.Report(__LINE__);
 }
 
 } // namespace StringTestNamespace

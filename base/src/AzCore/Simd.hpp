31254f8e759968a442ea78df18c0617bdaf63846
SingularityAzure <singularity.haynes@gmail.com>
2023-02-08 00:05:42 -0500

    Define lower SIMD specs in MSVC

--- base/src/AzCore/Simd.hpp
+++ base/src/AzCore/Simd.hpp
@@ -7,6 +7,29 @@
 #ifndef AZCORE_SIMD_HPP
 #define AZCORE_SIMD_HPP
 
+#ifdef _MSC_VER
+#ifdef __AVX__
+#ifndef __SSE__
+#define __SSE__ 1
+#endif
+#ifndef __SSE2__
+#define __SSE2__ 1
+#endif
+#ifndef __SSE3__
+#define __SSE3__ 1
+#endif
+#ifndef __SSSE3__
+#define __SSSE3__ 1
+#endif
+#ifndef __SSE4_1__
+#define __SSE4_1__ 1
+#endif
+#ifndef __SSE4_2__
+#define __SSE4_2__ 1
+#endif
+#endif
+#endif
+
 #if __SSE2__
 #include "SIMD/SimdSSE2.hpp"
 #else
